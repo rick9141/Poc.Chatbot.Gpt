aa7b213e1e32895c33abe541312d021a0f908553
rick9141 <gavirate.henrique@gmail.com>
2023-04-04 22:51:11 -0300

    Add inteface integration

---
+++ Api/Poc.Chatbot.Gpt.Infrastructure/Integrations/IChatGptService.cs
@@ -0,0 +1,13 @@
+﻿using RestEase;
+using System.Net.Http.Headers;
+
+namespace Poc.Chatbot.Gpt.Infrastructure.Integrations
+{
+    public interface IChatGptService
+    {
+        [Header("Authorization")]
+        AuthenticationHeaderValue Authorization { get; set; }
+
+
+    }
+}
